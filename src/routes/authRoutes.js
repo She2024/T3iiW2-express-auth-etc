6adc3d0595361cb32bbcebdfa97ac05aba8c4bac
she2024 <bullybluedog@hotmail.com>
2025-01-21 20:15:45 +1000

    feat: added controllers

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,13 @@
+
+const express = require("express");
+const { registerUser, loginUser } = require("../controllers/authController");
+
+const router = express.Router();
+
+// POST localhost:5000/api/auth/register
+router.post("/register", registerUser);
+
+// POST localhost:5000/api/auth/login
+router.post("/login", loginUser);
+
+module.exports = router;
